4ea6bc1598af01474f78276d3ad4459e14259d3a
Ron <ron@wymanfineart.co.uk>
2025-05-13 08:26:59 +0100

    Add MySQL database

--- mysite/settings.py
+++ mysite/settings.py
@@ -75,11 +75,21 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'restaurant_db',
+        'USER': 'root',
+        'PASSWORD': '1111',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
