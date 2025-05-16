e04f52cd08e517c35ce5b86c9afc45ae055c656f
Ron <ron@wymanfineart.co.uk>
2025-05-16 20:56:40 +0100

    Add list_display and earch_fields.

--- restaurant/models.py
+++ restaurant/models.py
@@ -9,7 +9,7 @@ class Meal(models.Model):
     # Store the meal price.
     price = models.DecimalField("Price (£)", max_digits=10, decimal_places=2)
     # Availability of the meal..
-    available = models.BooleanField(default=False)
+    available = models.BooleanField("Online Availability", default=False)
 
     def __str__(self):
         return f'{self.description}'
