3e85cb073ae9e282c1effda09a7b73144dafb94d
Ron <ron@wymanfineart.co.uk>
2025-05-18 18:49:04 +0100

    Test for GET request before returning meals as response.

--- restaurant/views.py
+++ restaurant/views.py
@@ -2,10 +2,15 @@ from http import HTTPStatus
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Meal
 
 # Create your views here.
 
 def index(request):
     #Do something.
+    if request.method == 'GET':
+        meals = Meal.objects.all()
+        return HttpResponse(meals)
     #return HttpResponse(200)
-    return HttpResponse(f"HTTP Response: {HTTPStatus.OK}")
+    #return HttpResponse(f"HTTP Response: {HTTPStatus.OK}")
+    return HttpResponse(f"HTTP Response: {HTTPStatus.BAD_REQUEST}")
