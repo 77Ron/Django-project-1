5b55953cd30bd7dd7098d5c9886d888b3f331a22
Ron <ron@wymanfineart.co.uk>
2025-05-18 18:15:05 +0100

    Add index view.

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,3 +1,11 @@
+from http import HTTPStatus
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+def index(request):
+    #Do something.
+    #return HttpResponse(200)
+    return HttpResponse(f"HTTP Response: {HTTPStatus.OK}")
