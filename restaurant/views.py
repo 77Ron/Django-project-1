322cd0e8dcd5ec2030589172010e8196cdbd1c48
Ron <ron@wymanfineart.co.uk>
2025-06-18 20:39:17 +0100

    Add meals to index page.

--- restaurant/views.py
+++ restaurant/views.py
@@ -9,7 +9,26 @@ from .models import Meal
 def index(request):
     #Do something.
     if request.method == 'GET':
-        meals = Meal.objects.all()
+        cnt = 0
+        # To store the meals.
+        meals = []
+        # To control the meals.
+        temp_list =[]
+        # Collect all the meals from the database.
+        all_meals = Meal.objects.all()
+
+        for cnt in range(all_meals.count()):
+            # Append the meal to the temp list.
+            temp_list.append(all_meals[cnt])
+
+            # Append temp list to meals and reset it for every third meal/
+            if (cnt + 1) % 3 == 0 and (cnt + 1)  > 2:
+                meals.append(temp_list)
+                temp_list = []
+                
+        # If we have lagging mmeals, just append tthem to meals.     
+        if temp_list:
+            meals.append(temp_list)
 
         context ={
             'meals' : meals,
