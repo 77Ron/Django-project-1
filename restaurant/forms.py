108b0d5f471189c8a5009c65b4666bd01c3dc873
Ron <ron@wymanfineart.co.uk>
2025-07-11 09:23:49 +0100

    Customise login input

--- restaurant/forms.py
+++ restaurant/forms.py
@@ -2,5 +2,17 @@ from django import forms
 
 
 class UserLoginForm(forms.Form):
-    username = forms.CharField()
+    username = forms.CharField(
+        required=True,
+        widget=forms.TextInput(
+            attrs={'placeholder': 'Your Username',
+            'autocomplete': 'off'
+            }
+            )
+        )
     password = forms.CharField(required=True, widget=forms.PasswordInput())
+
+    show = forms.BooleanField(
+        label='Show Password',
+        widget=forms.CheckboxInput(),
+        required=False)
