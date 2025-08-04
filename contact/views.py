4959af7e98eeca38aacf16efabc8044536004a02
Ron <ron@wymanfineart.co.uk>
2025-08-04 01:52:15 +0100

    Add contact form email send.

--- contact/views.py
+++ contact/views.py
@@ -7,47 +7,27 @@ from django.contrib import messages
 from django.conf import settings
 
 def contact_view(request):
-
     if request.method == 'POST':
         form = ContactForm(request.POST)
-
         if form.is_valid():
-            name = form.cleaned_data['name']
-            email = form.cleaned_data['email']
-            subject = form.cleaned_data['subject']
-            message = form.cleaned_data['message']
-
-            full_message = f"Message from {name} ({email}):\m\n{message}"
-
-            send_mail(
-                subject = subject,
-                message = full_message,
-                from_email = settings.EMAIL_HOST_USER, 
-                #from_email = settings.DEFAULT_FROM_EMAIL,
-                recipient_list = [email],
-                fail_silently=False,
-                )
-
-            messages.success(request, 'Thank you for contacting us!')
-
-            form = ContactForm()
+            subject = 'New Contact Form Submission'
+            message = f"""
+            From: {form.cleaned_data['name']} <{form.cleaned_data['email']}>
             
-
-        """if form.is_valid():
-            cd = form.cleaned_data
-            send_mail(
-                cd['subject'],
-                f"Message from {cd['name']} ({cd['email']}):\n\n{cd['message']}",
-                settings.DEFAULT_FROM_EMAIL,
-                [settings.EMAIL_HOST_USER],
-                fail_silently=False,
-            )
-            messages.success(request, "Thank you for contacting us!")
-            return redirect('contact')"""
-        
+            Message:
+            {form.cleaned_data['message']}
+            """
+            from_email = form.cleaned_data['email']
+            recipient_list = [settings.EMAIL_HOST_USER]
+            #fail_silently = False
+            auth_password = [settings.EMAIL_HOST_PASSWORD]
+            
+            send_mail(subject, message, from_email, recipient_list, auth_password)
+            
+            messages.success(request, 'Thank you for contacting us!')
+            
+            return redirect('contact')
     else:
         form = ContactForm()
 
     return render(request, template_name='contact/contact.html', context={'form': form})
-
-    #return render(request, 'contact/contact.html', {'form': form})
--- mysite/settings.py
+++ mysite/settings.py
@@ -11,8 +11,12 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-#from decouple import config
+
 import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -150,13 +154,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'  # Use your SMTP server
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD') 
 
-#EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-#EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')  # Use environment variables in production!
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD') 
-
-DEFAULT_FROM_EMAIL = 'Django App'
 
 # python manage.py collectstatic.
 
